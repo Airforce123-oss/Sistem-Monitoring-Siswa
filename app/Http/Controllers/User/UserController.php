7c36e1f631cc4088d80311c06db838bcfdc06978
Muh. Haikal Hanis <75037509+Airforce123-oss@users.noreply.github.com>
2024-05-09 00:27:58 +0800

    firstt

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+
+    }
+}
---
+++ resources/js/Pages/Admin/DataSiswa.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Data Siswa</h1>
+</template>
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+onMounted(() => {
+    initFlowbite();
+});
+</script>
